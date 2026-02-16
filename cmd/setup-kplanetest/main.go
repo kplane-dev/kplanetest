ffb5860385e68843ce30b4d26990476d40f5f18d
Zach Smith <zachdsmith2015@gmail.com>
2026-02-16 12:27:36 -0800

    chore: bump apiserver

--- cmd/setup-kplanetest/main.go
+++ cmd/setup-kplanetest/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	defaultVersion = "v0.0.6"
+	defaultVersion = "v0.0.8"
 	modulePath     = "github.com/kplane-dev/apiserver"
 	binaryName     = "kplane-apiserver"
 )
--- kplane_backend.go
+++ kplane_backend.go
@@ -35,7 +35,7 @@ import (
 const (
 	defaultRootControlPlane = "root"
 	kplaneReadyTimeout      = 60 * time.Second
-	kplaneModuleVersion     = "v0.0.6"
+	kplaneModuleVersion     = "v0.0.8"
 	kplaneAssetsEnv         = "KPLANETEST_ASSETS"
 	kplaneBinaryEnv         = "KPLANETEST_APISERVER_BINARY"
 )
